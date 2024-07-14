ec9a8c4f84870ae5868c3d2509c459828f7a2e8f
EngiBeerHub <engibeer0422@gmail.com>
2024-07-14 13:46:43 +0900

    第5章 原則をあえて破るとき

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -9,7 +9,6 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
-
         assertEquals(new Dollar(15), five.times(3));
     }
 
@@ -17,5 +16,12 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
